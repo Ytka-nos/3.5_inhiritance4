f8a15429442fc8d068fc8808f5cd665278daa27d
Mariia Sheynova <yellow_kitchen@mail.ru>
2021-10-29 18:00:48 +0300

    Commit for new fail for ci

--- src/test/java/ru/netology/ProductManagerTest.java
+++ src/test/java/ru/netology/ProductManagerTest.java
@@ -1,4 +1,5 @@
 package ru.netology;
+
 import org.junit.jupiter.api.Test;
 import ru.netology.domain.Book;
 import ru.netology.domain.Product;
@@ -11,13 +12,12 @@ public class ProductManagerTest {
     @Test
     public void successSearchFromMany() {
         ProductManager manager = new ProductManager();
-        Product first = new Book(1, "first", 10,"author1" );
-        Product second = new Book(2,"second",10,"author2");
+        Product first = new Book(1, "first", 10, "author1");
+        Product second = new Book(2, "second", 10, "author2");
         Product third = new Smartphone(3, "third", 30, "manufacture1");
         manager.add(first);
         manager.add(second);
         manager.add(third);
-
 
 
         Product[] actual = manager.searchBy("fir");
@@ -25,11 +25,10 @@ public class ProductManagerTest {
 
         assertArrayEquals(expected, actual);
     }
+
     @Test
     public void successSearchFromOne() {
         ProductManager manager = new ProductManager();
-        Product first = new Book(1, "first", 10,"author1" );
-        Product second = new Book(2,"second",10,"author2");
         Product third = new Smartphone(3, "third", 30, "manufacture1");
         manager.add(third);
 
@@ -43,10 +42,6 @@ public class ProductManagerTest {
     @Test
     public void failSearchFromNothing() {
         ProductManager manager = new ProductManager();
-        Product first = new Book(1, "first", 10,"author1" );
-        Product second = new Book(2,"second",10,"author2");
-        Product third = new Smartphone(3, "third", 30, "manufacture1");
-
 
         Product[] actual = manager.searchBy("fir");
         Product[] expected = new Product[]{};
@@ -57,13 +52,12 @@ public class ProductManagerTest {
     @Test
     public void failSearchFromMany() {
         ProductManager manager = new ProductManager();
-        Product first = new Book(1, "first", 10,"author1" );
-        Product second = new Book(2,"second",10,"author2");
+        Product first = new Book(1, "first", 10, "author1");
+        Product second = new Book(2, "second", 10, "author2");
         Product third = new Smartphone(3, "third", 30, "manufacture1");
         manager.add(first);
         manager.add(second);
         manager.add(third);
-
 
 
         Product[] actual = manager.searchBy("aaa");
@@ -73,16 +67,12 @@ public class ProductManagerTest {
     }
 
 
-
     @Test
     public void failSearchFromOne() {
         ProductManager manager = new ProductManager();
-        Product first = new Book(1, "first", 10,"author1" );
-        Product second = new Book(2,"second",10,"author2");
         Product third = new Smartphone(3, "third", 30, "manufacture1");
 
         manager.add(third);
-
 
 
         Product[] actual = manager.searchBy("fir");
@@ -90,11 +80,12 @@ public class ProductManagerTest {
 
         assertArrayEquals(expected, actual);
     }
+
     @Test
     public void successSearchAuthorFindTwo() {
         ProductManager manager = new ProductManager();
-        Product first = new Book(1, "first", 10,"author1" );
-        Product second = new Book(2,"second",10,"author2");
+        Product first = new Book(1, "first", 10, "author1");
+        Product second = new Book(2, "second", 10, "author2");
         Product third = new Smartphone(3, "third", 30, "manufacture1");
 
         manager.add(first);
@@ -102,9 +93,8 @@ public class ProductManagerTest {
         manager.add(third);
 
 
-
         Product[] actual = manager.searchBy("au");
-        Product[] expected = new Product[]{first,second};
+        Product[] expected = new Product[]{first, second};
 
         assertArrayEquals(expected, actual);
     }
@@ -112,14 +102,13 @@ public class ProductManagerTest {
     @Test
     public void successSearchAManufacture() {
         ProductManager manager = new ProductManager();
-        Product first = new Book(1, "first", 10,"author1" );
-        Product second = new Book(2,"second",10,"author2");
+        Product first = new Book(1, "first", 10, "author1");
+        Product second = new Book(2, "second", 10, "author2");
         Product third = new Smartphone(3, "third", 30, "manufacture1");
 
         manager.add(first);
         manager.add(second);
         manager.add(third);
-
 
 
         Product[] actual = manager.searchBy("ma");
