4fd1649e67fd5577e1c0d0990cc08e99bb04720f
Mariia Sheynova <yellow_kitchen@mail.ru>
2021-11-02 21:39:43 +0300

    Final commit

--- src/test/java/ru/netology/ProductManagerTest.java
+++ src/test/java/ru/netology/ProductManagerTest.java
@@ -14,7 +14,7 @@ public class ProductManagerTest {
     public void successSearchFromMany() {
 
         ProductRepository rep = new ProductRepository();
-        ProductManager manager = new ProductManager(ProductRepository rep);
+        ProductManager manager = new ProductManager(rep);
         Product first = new Book(1, "first", 10, "author1");
         Product second = new Book(2, "second", 10, "author2");
         Product third = new Smartphone(3, "third", 30, "manufacture1");
@@ -29,95 +29,101 @@ public class ProductManagerTest {
         assertArrayEquals(expected, actual);
     }
 
-//    @Test
-//    public void successSearchFromOne() {
-//        ProductManager manager = new ProductManager();
-//        Product third = new Smartphone(3, "third", 30, "manufacture1");
-//        manager.add(third);
-//
-//
-//        Product[] actual = manager.searchBy("ir");
-//        Product[] expected = new Product[]{third};
-//
-//        assertArrayEquals(expected, actual);
-//    }
-//
-//    @Test
-//    public void failSearchFromNothing() {
-//        ProductManager manager = new ProductManager();
-//
-//        Product[] actual = manager.searchBy("fir");
-//        Product[] expected = new Product[]{};
-//
-//        assertArrayEquals(expected, actual);
-//    }
-//
-//    @Test
-//    public void failSearchFromMany() {
-//        ProductManager manager = new ProductManager();
-//        Product first = new Book(1, "first", 10, "author1");
-//        Product second = new Book(2, "second", 10, "author2");
-//        Product third = new Smartphone(3, "third", 30, "manufacture1");
-//        manager.add(first);
-//        manager.add(second);
-//        manager.add(third);
-//
-//
-//        Product[] actual = manager.searchBy("aaa");
-//        Product[] expected = new Product[]{};
-//
-//        assertArrayEquals(expected, actual);
-//    }
-//
-//
-//    @Test
-//    public void failSearchFromOne() {
-//        ProductManager manager = new ProductManager();
-//        Product third = new Smartphone(3, "third", 30, "manufacture1");
-//
-//        manager.add(third);
-//
-//
-//        Product[] actual = manager.searchBy("fir");
-//        Product[] expected = new Product[]{};
-//
-//        assertArrayEquals(expected, actual);
-//    }
-//
-//    @Test
-//    public void successSearchAuthorFindTwo() {
-//        ProductManager manager = new ProductManager();
-//        Product first = new Book(1, "first", 10, "author1");
-//        Product second = new Book(2, "second", 10, "author2");
-//        Product third = new Smartphone(3, "third", 30, "manufacture1");
-//
-//        manager.add(first);
-//        manager.add(second);
-//        manager.add(third);
-//
-//
-//        Product[] actual = manager.searchBy("au");
-//        Product[] expected = new Product[]{first, second};
-//
-//        assertArrayEquals(expected, actual);
-//    }
-//
-//    @Test
-//    public void successSearchAManufacture() {
-//        ProductManager manager = new ProductManager();
-//        Product first = new Book(1, "first", 10, "author1");
-//        Product second = new Book(2, "second", 10, "author2");
-//        Product third = new Smartphone(3, "third", 30, "manufacture1");
-//
-//        manager.add(first);
-//        manager.add(second);
-//        manager.add(third);
-//
-//
-//        Product[] actual = manager.searchBy("ma");
-//        Product[] expected = new Product[]{third};
-//
-//        assertArrayEquals(expected, actual);
-//    }
-//
+    @Test
+    public void successSearchFromOne() {
+        ProductRepository rep = new ProductRepository();
+        ProductManager manager = new ProductManager(rep);
+        Product third = new Smartphone(3, "third", 30, "manufacture1");
+        manager.add(third);
+
+
+        Product[] actual = manager.searchBy("ir");
+        Product[] expected = new Product[]{third};
+
+        assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    public void failSearchFromNothing() {
+        ProductRepository rep = new ProductRepository();
+        ProductManager manager = new ProductManager(rep);
+
+        Product[] actual = manager.searchBy("fir");
+        Product[] expected = new Product[]{};
+
+        assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    public void failSearchFromMany() {
+        ProductRepository rep = new ProductRepository();
+        ProductManager manager = new ProductManager(rep);
+        Product first = new Book(1, "first", 10, "author1");
+        Product second = new Book(2, "second", 10, "author2");
+        Product third = new Smartphone(3, "third", 30, "manufacture1");
+        manager.add(first);
+        manager.add(second);
+        manager.add(third);
+
+
+        Product[] actual = manager.searchBy("aaa");
+        Product[] expected = new Product[]{};
+
+        assertArrayEquals(expected, actual);
+    }
+
+
+    @Test
+    public void failSearchFromOne() {
+        ProductRepository rep = new ProductRepository();
+        ProductManager manager = new ProductManager(rep);
+        Product third = new Smartphone(3, "third", 30, "manufacture1");
+
+        manager.add(third);
+
+
+        Product[] actual = manager.searchBy("fir");
+        Product[] expected = new Product[]{};
+
+        assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    public void successSearchAuthorFindTwo() {
+        ProductRepository rep = new ProductRepository();
+        ProductManager manager = new ProductManager(rep);
+        Product first = new Book(1, "first", 10, "author1");
+        Product second = new Book(2, "second", 10, "author2");
+        Product third = new Smartphone(3, "third", 30, "manufacture1");
+
+        manager.add(first);
+        manager.add(second);
+        manager.add(third);
+
+
+        Product[] actual = manager.searchBy("au");
+        Product[] expected = new Product[]{first, second};
+
+        assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    public void successSearchAManufacture() {
+        ProductRepository rep = new ProductRepository();
+        ProductManager manager = new ProductManager(rep);
+        Product first = new Book(1, "first", 10, "author1");
+        Product second = new Book(2, "second", 10, "author2");
+        Product third = new Smartphone(3, "third", 30, "manufacture1");
+
+        manager.add(first);
+        manager.add(second);
+        manager.add(third);
+
+
+        Product[] actual = manager.searchBy("ma");
+        Product[] expected = new Product[]{third};
+
+        assertArrayEquals(expected, actual);
+    }
+
 }
